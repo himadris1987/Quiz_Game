ac66310b531f8dcc66521b437dc4f3e9000f1133
Himadri <himadris@gmail.com>
2020-04-25 14:15:20 -0700

    updates

--- quiz.js
+++ quiz.js
@@ -63,7 +63,7 @@ function renderQuestion(){
     let q = questions[runningQuestion];
     
     question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
+    //qImg.innerHTML = "<img src="+ q.imgSrc +">";
     choiceA.innerHTML = q.choiceA;
     choiceB.innerHTML = q.choiceB;
     choiceC.innerHTML = q.choiceC;
